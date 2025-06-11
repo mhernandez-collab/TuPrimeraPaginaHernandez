52476401611c0bb56ac537d40489c76a56007ceb
Martin Hernandez <mhernandez@cbpi.com.mx>
2025-06-11 15:43:40 -0600

    Primer commit - Proyecto Django blog con MVT

--- blog/views.py
+++ blog/views.py
@@ -3,28 +3,28 @@ from .forms import AuthorForm, CategoryForm, PostForm, SearchForm
 from .models import Post
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'blog/home.html')
 
 def create_author(request):
     form = AuthorForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('home')
-    return render(request, 'create_author.html', {'form': form})
+    return render(request, 'blog/create_author.html', {'form': form})
 
 def create_category(request):
     form = CategoryForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('home')
-    return render(request, 'create_category.html', {'form': form})
+    return render(request, 'blog/create_category.html', {'form': form})
 
 def create_post(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('home')
-    return render(request, 'create_post.html', {'form': form})
+    return render(request, 'blog/create_post.html', {'form': form})
 
 def search_post(request):
     results = []
@@ -35,4 +35,4 @@ def search_post(request):
             results = Post.objects.filter(title__icontains=query)
     else:
         form = SearchForm()
-    return render(request, 'search.html', {'form': form, 'results': results})
+    return render(request, 'blog/search.html', {'form': form, 'results': results})
